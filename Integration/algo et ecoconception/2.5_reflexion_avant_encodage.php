5ab62a669110a4e6d840ff184906784f85655dcd
59011-14-05 <yoann.deco@gmail.com>
2022-10-21 11:08:01 +0200

    exercice 2.5 + création fichier exercice 3.1

--- Integration/algo et ecoconception/2.5_reflexion_avant_encodage.php
+++ Integration/algo et ecoconception/2.5_reflexion_avant_encodage.php
@@ -1,2 +1,28 @@
 <?php
 
+$age = readline("Veuillez saisir votre âge : ");
+$permis = readline("Depuis quand avez-vous votre permis ? ");
+$accidents = readline("Combien d'accidents avez-vous eu avec votre véhicule ?");
+$fidelite = readline("Êtes-vous membre depuis plus de 1 an ?");
+
+$points = 0;
+
+if ($accidents < 3) {
+    if ($age > 25) {
+        $points++;
+    }
+
+    if ($permis > 2) {
+        $points++;
+    }
+
+    if ($fidelite === "oui") {
+        $points++;
+    }
+
+    $points -= $accidents;
+    echo $points;
+    
+}else {
+    echo "vous ne pouvez pas être assuré.";
+}
