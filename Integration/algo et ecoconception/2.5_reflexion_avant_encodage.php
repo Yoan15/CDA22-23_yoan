f88b5050bc48fc7a1e4a5598ead5f23d9f78ec28
59011-14-05 <yoann.deco@gmail.com>
2022-10-21 11:21:13 +0200

    continuation exercice 2.5

--- Integration/algo et ecoconception/2.5_reflexion_avant_encodage.php
+++ Integration/algo et ecoconception/2.5_reflexion_avant_encodage.php
@@ -6,6 +6,11 @@ $accidents = readline("Combien d'accidents avez-vous eu avec votre véhicule ?")
 $fidelite = readline("Êtes-vous membre depuis plus de 1 an ?");
 
 $points = 0;
+$tarifs = array(4 => "Bleu", 
+                3 => "Vert", 
+                2 => "Orange", 
+                1 => "Rouge",
+                0 => "Refus");
 
 if ($accidents < 3) {
     if ($age > 25) {
@@ -22,7 +27,9 @@ if ($accidents < 3) {
 
     $points -= $accidents;
     echo $points;
+
     
+
 }else {
     echo "vous ne pouvez pas être assuré.";
 }
