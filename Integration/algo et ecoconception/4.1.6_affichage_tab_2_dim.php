bdf7aaa190d5007538da385c8c474008f9632795
Yoan Deconinck <yoann.deco@gmail.com>
2022-10-24 19:56:30 +0200

    4.1.6 - changement valeur ligne pour plus de cohérence

--- Integration/algo et ecoconception/4.1.6_affichage_tab_2_dim.php
+++ Integration/algo et ecoconception/4.1.6_affichage_tab_2_dim.php
@@ -5,9 +5,9 @@ $tab = [[1,2,3],[4,5,6]];
 function AfficherTableauDoubleDim($tab){
     var_dump($tab);
     foreach ($tab as $numLigne => $ligne) {
-        echo $numLigne;
+        echo "| " . ($numLigne+1) . " "; //numéro ligne +1 pour donner la valeur 1 à la ligne 1 a la place de la valeur 0
         foreach ($ligne as $case) {
-            echo "| ". $case . " "; // ajout d'un pipe à gauche de chaque valeur
+            echo "| " . $case . " "; // ajout d'un pipe à gauche de chaque valeur
         }
         echo "|\n"; // ajout d'un pipe à la fin de chaque ligne
     }
