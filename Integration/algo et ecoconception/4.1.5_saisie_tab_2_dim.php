f375fbe687a0d4d9a8f95149c7808888902d916f
59011-14-05 <yoann.deco@gmail.com>
2022-10-24 16:16:16 +0200

    exercice 4.1.5

--- Integration/algo et ecoconception/4.1.5_saisie_tab_2_dim.php
+++ Integration/algo et ecoconception/4.1.5_saisie_tab_2_dim.php
@@ -4,10 +4,15 @@
 
 function SaisieTabDouble(){
     $tab = [];
-    $nbValeurs = readline("Veuillez saisir le nombre d'objets à ");
+    $nbValeurs = readline("Veuillez saisir le nombre d'objets à créer : ");
     for ($x=0; $x < $nbValeurs; $x++) { 
-        for ($y=0; $y < ; $y++) { 
-            
+        $objet = readline("Veuillez saisir le nom de votre objet : ");
+        $tab[] = $objet;
+        $nbParametres = readline("Veuillez saisir le nombre de paramètre : ");
+        for ($y=0; $y < $nbParametres; $y++) { 
+            $parametres = readline("Veuillez saisir les parametres de votre objet : ");
+            $tab[$objet][] = $parametres;
+            var_dump($tab);
         }
     }
 }
