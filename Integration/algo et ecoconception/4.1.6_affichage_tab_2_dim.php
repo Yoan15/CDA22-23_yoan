0f8356232bd1911075efb53d95156ab6a3fb406b
Yoan Deconinck <yoann.deco@gmail.com>
2022-10-24 20:14:52 +0200

    4.1.6 - affichage des noms cols

--- Integration/algo et ecoconception/4.1.6_affichage_tab_2_dim.php
+++ Integration/algo et ecoconception/4.1.6_affichage_tab_2_dim.php
@@ -6,13 +6,15 @@ function AfficherTableauDoubleDim($tab){
     var_dump($tab);
 
     $tirets = "----"; //pour prendre en compte le numéro de la ligne
+    $nomCol = "    "; 
     $nbCol = count($tab[0]); //On compte le nombre de colonnes
 
     for ($i=0; $i < $nbCol; $i++) { 
         $tirets .= "----";
+        $nomCol .= "| " . mb_chr(65+$i) . " "; // On utilise le code ASCII correspondant à la colonne
     }
     $tirets .= "\n"; //retour à la ligne après les tirets
-
+    echo $nomCol;
     foreach ($tab as $numLigne => $ligne) {
         echo "| " . ($numLigne+1) . " "; //numéro ligne +1 pour donner la valeur 1 à la ligne 1 a la place de la valeur 0
         foreach ($ligne as $case) {
