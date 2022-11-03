e5911e4bd557d1328d887bcf119cdb41c9016191
59011-14-05 <yoann.deco@gmail.com>
2022-11-03 14:30:46 +0100

    tests avec le elementById réussi

--- Integration/algo et ecoconception/Exercices JS/exercice 04/script.js
+++ Integration/algo et ecoconception/Exercices JS/exercice 04/script.js
@@ -4,18 +4,48 @@
 // pu2 = document.getElementById("pu2");
 // p1 = document.getElementById("p1");
 // p2 = document.getElementById("p2");
-lesQuantites = document.querySelectorAll("[data-quantite]");
-lesPrixUnitaires = document.querySelectorAll("[data-prixuni]");
-lesPrix = document.querySelectorAll("[data-prix]");
+calcul1 = document.querySelectorAll("[data-calcul1]");
+prix1 = document.querySelectorAll("[data-prix1]");
+calcul2 = document.querySelectorAll("[data-calcul2]");
+prix2 = document.querySelectorAll("[data-prix2]");
 prixTotal = document.querySelectorAll("[data-prixtotal]");
 
-lesQuantites.foreach(element =>
-    element.addEventListener("blur", ));
+calcul1.forEach(element =>{
+    element.addEventListener("blur", calculPrix1);
+    console.log(element);
+});
 
-function calculPrix1() {
+function calculPrix1(event) {
+    
+    /*********TESTS********** */
+    // qte1 = document.getElementById("qte1").value; //arrive a récupérer la valeur
+    // console.log(qte1);
+    // pu1 = document.getElementById("pu1").value; //arrive a récupérer la valeur
+    // console.log(pu1);
+    // p1 = qte1*pu1;
+    // console.log(p1);
+    // document.getElementById("p1").value = p1; //arrive a mettre la valeur dans l'input
+
+
+    // qte1 = event.target;
+    // qte1 = calcul1.getAttribute("data-calcul1")=="qte1";
+    // console.log(qte1);
+}
+
+calcul2.forEach(element =>{
+    element.addEventListener("blur", calculPrix2);
+    console.log(element);
+});
+
+function calculPrix2(event) {
     
 }
 
+function disableTotal(){
+    pt = document.getElementById("pt").disabled = true;
+}
+
+/********REGEX********** */
 // var validation = new RegExp("/^[0-9]*$/");
 // var resultat = validation.test(qt1);
 // console.log(resultat);
