7aa414ad4368ad08c033ff7120d1f39f43d3234b
Yoan Deconinck <yoann.deco@gmail.com>
2022-11-03 21:42:08 +0100

    ex 04 refait + creation fichiers ex 06 et 07

--- Integration/algo et ecoconception/Exercices JS/exercice 04/exercice 04 bis/script.js
+++ Integration/algo et ecoconception/Exercices JS/exercice 04/exercice 04 bis/script.js
@@ -1,4 +1,52 @@
-quantites = document.querySelectorAll(".qte");
-prixUnitaires = document.querySelectorAll(".prix");
-prixTotal = document.querySelectorAll(".total");
+// grille = document.getElementById("grille");
+// template = document.getElementsByTagName("template")[0];
+// for (let i = 0; i < 10; i++) {
+//     const element = template.content.cloneNode(true);
+//     grille.appendChild(element);
+// }
 
+grille = document.getElementById("grille");
+template = document.getElementsByTagName("template")[0];
+for (let i = 0; i < 1; i++) {
+    const element = template.content.cloneNode(true);
+    grille.appendChild(element);
+}
+
+quantites = document.querySelectorAll(".qte");
+prixUnitaires = document.querySelectorAll(".pu");
+prix = document.querySelectorAll(".prix");
+
+quantites.forEach(element=>{
+    element.addEventListener("input", calcul);
+});
+prixUnitaires.forEach(element=>{
+    element.addEventListener("input", calcul);
+});
+
+function calcul(event)
+{
+    input = event.target;
+    if (input.classList.contains("qte")) 
+    {
+        index = Array.from(quantites).indexOf(input);    
+    } 
+    else
+    {
+        index = Array.from(prixUnitaires).indexOf(input);
+    }
+
+    if (quantites[index].value!="" && prixUnitaires[index].value!="") 
+    {
+        prix[index].value = quantites[index].value * prixUnitaires[index].value;    
+    }
+    calculTotal();
+}
+
+function calculTotal(event)
+{
+    somme=0;
+    prix.forEach(element=>{
+        somme += element.value*1
+    });
+    document.getElementsByClassName("total")[0].value = somme;
+}
