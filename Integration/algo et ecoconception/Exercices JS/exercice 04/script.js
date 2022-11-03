ba4a5c23c9c78c05e4e7afb47ae7e148ccf9aa5b
59011-14-05 <yoann.deco@gmail.com>
2022-11-03 15:16:38 +0100

    avancement

--- Integration/algo et ecoconception/Exercices JS/exercice 04/script.js
+++ Integration/algo et ecoconception/Exercices JS/exercice 04/script.js
@@ -1,13 +1,7 @@
-// qt1 = document.getElementById("qt1");
-// pu1 = document.getElementById("pu1");
-// qt2 = document.getElementById("qt2");
-// pu2 = document.getElementById("pu2");
-// p1 = document.getElementById("p1");
-// p2 = document.getElementById("p2");
 calcul1 = document.querySelectorAll("[data-calcul1]");
-prix1 = document.querySelectorAll("[data-prix1]");
+//prix1 = document.querySelectorAll("[data-prix1]");
 calcul2 = document.querySelectorAll("[data-calcul2]");
-prix2 = document.querySelectorAll("[data-prix2]");
+//prix2 = document.querySelectorAll("[data-prix2]");
 prixTotal = document.querySelectorAll("[data-prixtotal]");
 
 calcul1.forEach(element =>{
@@ -16,20 +10,24 @@ calcul1.forEach(element =>{
 });
 
 function calculPrix1(event) {
-    
+
     /*********TESTS********** */
-    // qte1 = document.getElementById("qte1").value; //arrive a récupérer la valeur
-    // console.log(qte1);
-    // pu1 = document.getElementById("pu1").value; //arrive a récupérer la valeur
-    // console.log(pu1);
-    // p1 = qte1*pu1;
-    // console.log(p1);
-    // document.getElementById("p1").value = p1; //arrive a mettre la valeur dans l'input
+    qte1 = document.getElementById("qte1").value; //arrive a récupérer la valeur
+    console.log(qte1);
+    pu1 = document.getElementById("pu1").value; //arrive a récupérer la valeur
+    console.log(pu1);
+    p1 = parseInt(qte1)*parseFloat(pu1);
+    console.log(p1);
+    document.getElementById("p1").value = p1; //arrive a mettre la valeur dans l'input
 
 
-    // qte1 = event.target;
-    // qte1 = calcul1.getAttribute("data-calcul1")=="qte1";
-    // console.log(qte1);
+    // calcul1 = event.target;
+    // if (calcul1.getAttribute("data-calcul1")) {
+    //     qte1 = calcul1.value;
+    //     console.log(qte1);
+    //     pu1 = calcul1.nextElementSibling.value;
+    //     console.log(pu1);
+    // }
 }
 
 calcul2.forEach(element =>{
@@ -39,6 +37,31 @@ calcul2.forEach(element =>{
 
 function calculPrix2(event) {
     
+    /*********TESTS********** */
+    qte2 = document.getElementById("qte2").value; //arrive a récupérer la valeur
+    console.log(qte2);
+    pu2 = document.getElementById("pu2").value; //arrive a récupérer la valeur
+    console.log(pu2);
+    p2 = parseInt(qte2)*parseFloat(pu2);
+    console.log(p2);
+    document.getElementById("p2").value = p2; //arrive a mettre la valeur dans l'input
+}
+
+prixTotal.forEach(element =>{
+    element.addEventListener("blur", calculPrixTotal);
+    console.log(element);
+});
+
+function calculPrixTotal(event) {
+    
+    /*********TESTS********** */
+    p1 = document.getElementById("p1").value; //arrive a récupérer la valeur
+    console.log(p1);
+    p2 = document.getElementById("p2").value; //arrive a récupérer la valeur
+    console.log(p2);
+    pt = parseFloat(p1) + parseFloat(p2);
+    console.log(pt);
+    document.getElementById("pt").value = pt; //arrive a mettre la valeur dans l'input
 }
 
 function disableTotal(){
