d55ab153746354707741f3e251ffad5208cd1bf7
59011-14-05 <yoann.deco@gmail.com>
2022-11-03 13:36:55 +0100

    avancement

--- Integration/algo et ecoconception/Exercices JS/exercice 04/script.js
+++ Integration/algo et ecoconception/Exercices JS/exercice 04/script.js
@@ -1,8 +1,21 @@
-qt1 = document.getElementById("qt1");
-pu1 = document.getElementById("pu1");
-qt2 = document.getElementById("qt1");
-pu2 = document.getElementById("pu2");
+// qt1 = document.getElementById("qt1");
+// pu1 = document.getElementById("pu1");
+// qt2 = document.getElementById("qt2");
+// pu2 = document.getElementById("pu2");
+// p1 = document.getElementById("p1");
+// p2 = document.getElementById("p2");
+lesQuantites = document.querySelectorAll("[data-quantite]");
+lesPrixUnitaires = document.querySelectorAll("[data-prixuni]");
+lesPrix = document.querySelectorAll("[data-prix]");
+prixTotal = document.querySelectorAll("[data-prixtotal]");
 
-var validation = new RegExp("/^[0-9]*$/");
-var resultat = validation.test(qt1);
-console.log(resultat);
+lesQuantites.foreach(element =>
+    element.addEventListener("blur", ));
+
+function calculPrix1() {
+    
+}
+
+// var validation = new RegExp("/^[0-9]*$/");
+// var resultat = validation.test(qt1);
+// console.log(resultat);
