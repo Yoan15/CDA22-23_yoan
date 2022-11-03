ad07e7257d05db22a72064805d3c43e304947443
59011-14-05 <yoann.deco@gmail.com>
2022-11-03 12:17:33 +0100

    début exercice 04

--- Integration/algo et ecoconception/Exercices JS/exercice 04/script.js
+++ Integration/algo et ecoconception/Exercices JS/exercice 04/script.js
@@ -3,3 +3,6 @@ pu1 = document.getElementById("pu1");
 qt2 = document.getElementById("qt1");
 pu2 = document.getElementById("pu2");
 
+var validation = new RegExp("/^[0-9]*$/");
+var resultat = validation.test(qt1);
+console.log(resultat);
