8901a852d2806685e2c726661d98c8483bd33763
59011-14-05 <yoann.deco@gmail.com>
2022-11-03 12:10:04 +0100

    03/11/2022 12:10:03,82

--- Integration/algo et ecoconception/Exercices JS/exercice 03/script.js
+++ Integration/algo et ecoconception/Exercices JS/exercice 03/script.js
@@ -1,4 +1,4 @@
 var element = document.getElementById("ampoule");
 element.addEventListener("click", function () {
-    this.src='ampoule_allumee.png';
+    (this.src.match("eteinte")) ? this.src='ampoule_allumee.png' : this.src='ampoule_eteinte.png';
 });
---
+++ Integration/algo et ecoconception/Exercices JS/exercice 04/script.js
@@ -0,0 +1,5 @@
+qt1 = document.getElementById("qt1");
+pu1 = document.getElementById("pu1");
+qt2 = document.getElementById("qt1");
+pu2 = document.getElementById("pu2");
+
