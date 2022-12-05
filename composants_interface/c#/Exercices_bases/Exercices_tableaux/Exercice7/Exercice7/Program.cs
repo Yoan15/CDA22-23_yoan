00baaa27d471a0def5216b01bfaf7546db25781d
Yoan15 <yoann.deco@gmail.com>
2022-12-05 16:43:57 +0100

    exercices c#

--- composants_interface/c#/Exercices_bases/Exercices_tableaux/Exercice7/Exercice7/Program.cs
+++ composants_interface/c#/Exercices_bases/Exercices_tableaux/Exercice7/Exercice7/Program.cs
@@ -8,10 +8,19 @@ namespace Exercice7
         {
             int[] T;
             int _nbATrouver;
+            int _trouve;
             T = new int[10] { 8, 45, 7, 10, 5, 6, 91, 2, 64, 71 };
             Console.WriteLine("Veuillez saisir un nombre à trouver : ");
             _nbATrouver = Convert.ToInt32(Console.ReadLine());
-            T.Find(T, _nbATrouver);
+            _trouve = Array.IndexOf(T, _nbATrouver);
+            if (_trouve == -1)
+            {
+                Console.WriteLine("Le nombre n'existe pas dans le tableau.");
+            }
+            else
+            {
+                Console.WriteLine("Le nombre " + _nbATrouver + " se trouve à l'index numéro : " + _trouve);
+            }
         }
     }
 }
