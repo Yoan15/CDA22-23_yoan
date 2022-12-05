e11ba9d5dd287604cc2d9b6f50060b93d2ab413a
Yoan15 <yoann.deco@gmail.com>
2022-12-05 17:10:15 +0100

    05/12/2022 17:10:11,21

--- composants_interface/c#/Exercices_bases/Exercices_sous_programme/Exercice1.2_1/Exercice1.2_1/Program.cs
+++ composants_interface/c#/Exercices_bases/Exercices_sous_programme/Exercice1.2_1/Exercice1.2_1/Program.cs
@@ -9,26 +9,44 @@ namespace Exercice1._2_1
             int n;
             Console.WriteLine("Veuillez saisir un nombre : ");
             n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Le nombre choisi est " + n);
+            Unites(n);
+            Dizaines(n);
         }
 
         public static int Unites(int n)
         {
-
+            if (n < 10)
+            {
+                Console.WriteLine("Le nombre comporte " + n + " unités.");
+            }
+            return n;
         }
 
         public static int Dizaines(int n)
         {
+            if (n > 10)
+            {
+                int n1 = n % 10;
+                Console.WriteLine(n);
+                Console.WriteLine("Le nombre comporte " + n + " dizaines.");
+            }
+            return n;
+        }
 
+        public static int Extrait(int n, int p)
+        {
+            return n;
         }
 
         public static int NbChiffres(int n)
         {
-
+            return n;
         }
 
         public static int SommeChiffres(int n)
         {
-
+            return n;
         }
     }
 }
