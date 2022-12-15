873a8bbd9f00a9f44d8d4c83d1b9907aa0746a15
Yoan15 <yoann.deco@gmail.com>
2022-12-15 12:10:08 +0100

    15/12/2022 12:10:06,07

---
+++ composants_interface/c#/WPF/Exercices/Exercice_produits_mw/Exercice_produits_mw/Produits.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Exercice_produits_mw
+{
+    public class Produits
+    {
+        public int IdProduit { get; set; }
+        public string NomProduit { get; set; }
+        public int Quantite { get; set; }
+
+        public Produits(int idProduit, string nomProduit, int quantite)
+        {
+            IdProduit = idProduit;
+            NomProduit = nomProduit;
+            Quantite = quantite;
+        }
+
+        private void produitsJson()
+        {
+            string filename = "produits.json";
+            string jsonString = File.ReadAllText(filename);
+            var produit = JsonSerializer.Deserialize<Produits>(jsonString);
+        }
+    }
+}
