6a7bd50ca881c6f25f692e577d4559683482b412
Yoan15 <yoann.deco@gmail.com>
2022-12-20 16:43:40 +0100

    Création projets a refaire + EntityBaseToModelRetry

---
+++ composants_interface/c#/EntityFramework/Introduction/EntityBaseToModelRetry/EntityBaseToModelRetry/Models/DbModels/Produit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EntityBaseToModelRetry.Models.DbModels
+{
+    public partial class Produit
+    {
+        public int Id { get; set; }
+        public string LibelleProduit { get; set; }
+        public string DescProduit { get; set; }
+        public int Quantite { get; set; }
+    }
+}
