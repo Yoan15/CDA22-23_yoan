5b526ee3671f7caf4e31ac508414031df74966fe
Yoan15 <yoann.deco@gmail.com>
2022-12-20 16:03:00 +0100

    Entity ModelToBase ajout commentaire

--- composants_interface/c#/EntityFramework/Introduction/EntityModelToBase/EntityModelToBase/Data/Personnes.cs
+++ composants_interface/c#/EntityFramework/Introduction/EntityModelToBase/EntityModelToBase/Data/Personnes.cs
@@ -9,8 +9,12 @@ namespace EntityModelToBase.Data
     public class Personnes
     {
         public int Id { get; set; }
+
+        // 2e façon de changer le type des données
         [MaxLength(50)]
         public string Prenom { get; set; }
+
+        
         [MaxLength(50)]
         public string Nom { get; set; }
         public int Age { get; set; }
