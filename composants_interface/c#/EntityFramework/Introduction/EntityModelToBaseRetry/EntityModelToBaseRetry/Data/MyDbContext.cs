13015fac2d1b06c0b5e003d1b759134ae09045e5
Yoan15 <yoann.deco@gmail.com>
2022-12-20 17:10:06 +0100

    20/12/2022 17:10:05,06

---
+++ composants_interface/c#/EntityFramework/Introduction/EntityModelToBaseRetry/EntityModelToBaseRetry/Data/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityModelToBaseRetry.Data
+{
+    public class MyDbContext:DbContext
+    {
+        public DbSet<Produits> Produits { get; set; }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModele
+    }
+}
---
+++ composants_interface/c#/EntityFramework/Introduction/EntityModelToBaseRetry/EntityModelToBaseRetry/Data/Produits.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityModelToBaseRetry.Data
+{
+    public class Produits
+    {
+        public int Id { get; set; }
+        public string Libelle { get; set; }
+        public string Desc { get; set; }
+        public int Prix { get; set; }
+    }
+}
