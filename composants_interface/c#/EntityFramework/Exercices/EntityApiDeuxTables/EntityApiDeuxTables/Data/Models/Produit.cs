dd7dae69fe68e3d92d393accdd1aedf1c77abf53
Yoan15 <yoann.deco@gmail.com>
2022-12-22 12:07:10 +0100

    génération POCO produits et categories

---
+++ composants_interface/c#/EntityFramework/Exercices/EntityApiDeuxTables/EntityApiDeuxTables/Data/Models/Produit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EntityApiDeuxTables.Data.Models
+{
+    public partial class Produit
+    {
+        public int Id { get; set; }
+        public string LibelleProduit { get; set; }
+        public int Qte { get; set; }
+        public int? IdCategorie { get; set; }
+    }
+}
