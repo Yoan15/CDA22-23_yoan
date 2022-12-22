d17e293491727bfa15357828fce80e89f69dd8fe
Yoan15 <yoann.deco@gmail.com>
2022-12-22 17:14:44 +0100

    GestionStock création context

--- composants_interface/c#/EntityFramework/Exercices/GestionStock/GestionStock/MyDbContext.cs
+++ composants_interface/c#/EntityFramework/Exercices/GestionStock/GestionStock/MyDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace GestionStock
 {
-    class MyDbContext
+    class MyDbContext:DbContext
     {
+        public MyDbContext(DbContextOptions<MyDbContext> options):base(options)
+        {
+
+        }
     }
 }
