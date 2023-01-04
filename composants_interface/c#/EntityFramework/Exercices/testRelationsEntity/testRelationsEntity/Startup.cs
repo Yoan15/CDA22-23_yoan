48bcb5f44390502971f2445b6edf7149ea485dd3
Yoan15 <yoann.deco@gmail.com>
2023-01-04 17:13:33 +0100

    ajout commentaire pour le patch dans le startup

--- composants_interface/c#/EntityFramework/Exercices/testRelationsEntity/testRelationsEntity/Startup.cs
+++ composants_interface/c#/EntityFramework/Exercices/testRelationsEntity/testRelationsEntity/Startup.cs
@@ -30,7 +30,7 @@ namespace testRelationsEntity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews().AddNewtonsoftJson();
+            services.AddControllersWithViews().AddNewtonsoftJson(); //pour le PATCH
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddDbContext<MyDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("Default")));
             services.AddTransient<PersonnesServices>();
