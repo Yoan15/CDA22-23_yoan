5d6ea00186fedb800badef7a909dda037e2111b6
Yoan15 <yoann.deco@gmail.com>
2023-01-04 10:23:46 +0100

    affichage liste personne et affichage personne par id

--- composants_interface/c#/EntityFramework/Exercices/testRelationsEntity/testRelationsEntity/Controllers/PersonnesController.cs
+++ composants_interface/c#/EntityFramework/Exercices/testRelationsEntity/testRelationsEntity/Controllers/PersonnesController.cs
@@ -4,13 +4,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using testRelationsEntity.Data.Dtos;
 using testRelationsEntity.Data.Services;
 
 namespace testRelationsEntity.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PersonnesController:ControllerBase
+    public class PersonnesController : ControllerBase
     {
         private readonly PersonnesServices _service;
         private readonly IMapper _mapper;
@@ -22,7 +23,23 @@ namespace testRelationsEntity.Controllers
         }
 
         //GET api/personnes
-        //[HttpGet]
+        [HttpGet]
+        public ActionResult<IEnumerable<PersonnesDTO>> getAllPersonnes()
+        {
+            var listePersonne = _service.GetAllPersonnes();
+            return Ok(_mapper.Map<IEnumerable<PersonnesDTO>>(listePersonne));
+        }
 
+        //GET api/personnes/{id}
+        [HttpGet("{id}", Name = "GetPersonneById")]
+        public ActionResult<PersonnesDTO> GetPersonneById(int id)
+        {
+            var personneItem = _service.GetPersonneById(id);
+            if (personneItem != null)
+            {
+                return Ok(_mapper.Map<PersonnesDTO>(personneItem));
+            }
+            return NotFound();
+        }
     }
 }
