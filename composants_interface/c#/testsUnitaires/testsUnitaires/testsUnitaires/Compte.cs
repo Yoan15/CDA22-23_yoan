6e3b6816ed45b18db59bc009b44ac9ca1e780a85
Yoan15 <yoann.deco@gmail.com>
2023-01-19 17:21:13 +0100

    test sandcastle

--- composants_interface/c#/testsUnitaires/testsUnitaires/testsUnitaires/Compte.cs
+++ composants_interface/c#/testsUnitaires/testsUnitaires/testsUnitaires/Compte.cs
@@ -6,20 +6,44 @@ using System.Threading.Tasks;
 
 namespace testsUnitaires
 {
+    /// <summary>
+    /// classe compte
+    /// </summary>
     public class Compte
     {
+        /// <summary>
+        /// getter et setter du client
+        /// </summary>
         public string Client { get; set; }
+        /// <summary>
+        /// getter et setter du solde
+        /// </summary>
         public double Solde { get; set; }
+        /// <summary>
+        /// constructeur du compte
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="solde"></param>
         public Compte(string client, double solde)
                 {
                     Client = client;
                     Solde = solde;
                 }
 
+        /// <summary>
+        /// constante qui affiche un message si le montant à débiter est plus élevé que le solde client
+        /// </summary>
         public const string DebitMontantSuperieurSoldeMessage = "Le montant à débiter est supérieur au solde";
 
+        /// <summary>
+        /// constante qui affiche un message si le montant est négatif
+        /// </summary>
         public const string DebitMontantNegatifMessage = "Le montant à débiter est négatif";
 
+        /// <summary>
+        /// Test sandcastle Debit
+        /// </summary>
+        /// <param name="montant"></param>
         public void Debit(double montant)
         {
             if (montant > Solde)
@@ -35,6 +59,10 @@ namespace testsUnitaires
             Solde += montant;
         }
 
+        /// <summary>
+        /// test sandcastle Credit
+        /// </summary>
+        /// <param name="montant"></param>
         public void Credit(double montant)
         {
             if (montant < 0)
