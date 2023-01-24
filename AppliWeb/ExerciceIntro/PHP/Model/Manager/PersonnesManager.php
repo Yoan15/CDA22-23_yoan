0dc809bccb80cfb21d7ebb7cba96f4a8111d6899
Yoan15 <yoann.deco@gmail.com>
2023-01-24 17:10:07 +0100

    24/01/2023 17:10:05,54

--- AppliWeb/ExerciceIntro/PHP/Model/Manager/PersonnesManager.php
+++ AppliWeb/ExerciceIntro/PHP/Model/Manager/PersonnesManager.php
@@ -15,6 +15,17 @@ class PersonnesManager
     }
 
     /**
+     * Permet de récupérer une personne par son ID
+     */
+    public static function GetPersonneById($id)
+    {
+        $stmt = DBConnect::getDb()->prepare("SELECT * FROM client WHERE id=?");
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    /**
      * Fonction qui permet d'ajouter une personne
      *
      * 
@@ -26,4 +37,27 @@ class PersonnesManager
         $stmt->bindParam(2, $prenom, PDO::PARAM_STR);
         $stmt->execute();
     }
+
+    /**
+     * Fonction qui permet de supprimer une personne de la BDD
+     */
+    public static function DeletePersonne($id)
+    {
+        $stmt = DBConnect::getDb()->prepare("DELETE FROM client WHERE id=?");
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+
+    /**
+     * Fontion qui permet de modifier une personne en BDD
+     */
+    public static function UpdatePersonne($nom, $prenom, $id)
+    {
+        $stmt = DBConnect::getDb()->prepare("UPDATE client SET 'nom'=?, 'prenom'=? WHERE 'id'=?");
+        $stmt->bindParam(1, $nom, PDO::PARAM_STR);
+        $stmt->bindParam(2, $prenom, PDO::PARAM_STR);
+        $stmt->bindParam(3, $id, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+    
 }
