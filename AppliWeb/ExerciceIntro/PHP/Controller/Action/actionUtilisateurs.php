7c162ab7bb23c1a37908f02940e88cdba2a2b758
Yoan15 <yoann.deco@gmail.com>
2023-02-07 12:14:16 +0100

    ajustement css + ajout actions user

--- AppliWeb/ExerciceIntro/PHP/Controller/Action/actionUtilisateurs.php
+++ AppliWeb/ExerciceIntro/PHP/Controller/Action/actionUtilisateurs.php
@@ -1,2 +1,21 @@
 <?php
 
+$elm = new Utilisateur($_POST);
+var_dump($elm,$_POST);
+
+switch ($_GET['mode']) {
+	case "ajouter": {
+		$elm = UtilisateursManager::AddUtilisateur($elm);
+		break;
+	}
+	case "modifier": {
+		$elm = UtilisateursManager::UpdateUtilisateur($elm, $elm->getIdUtilisateur());
+		break;
+	}
+	case "supprimer": {
+		$elm = UtilisateursManager::DeleteUtilisateur($elm);
+		break;
+	}
+}
+
+header("location:index.php?page=listeUtilisateur");
