9b6e0e6a6ce85e8d7a8c7ea0f24567228d7be1f3
Yoan15 <yoann.deco@gmail.com>
2023-02-09 16:29:50 +0100

    ajout commentaires

--- AppliWeb/ExerciceIntro/JS/scriptListeVille.js
+++ AppliWeb/ExerciceIntro/JS/scriptListeVille.js
@@ -13,14 +13,21 @@ if (page == 'formVille') {
 
     request.onreadystatechange = function (event) {
         if (this.readyState === XMLHttpRequest.DONE) {
+            //si le status est le code 200
             if (this.status === 200) {
-                console.log(this.responseText);
+                //console.log(this.responseText);
+                //on scinde les données reçues en réponse
                 reponse = JSON.parse(this.responseText);
+                //pour chaque élément on insert les noms et prénoms des personnes habitants dans la ville sélectionnée 
+                //dans une "div" à partir d'un "template" puis on retire la classe du template
                 reponse.forEach(element => {
                     template = document.getElementById("perso");
                     cible = document.getElementById("personne");
+                    //on clone le contenu du template
                     contenu = template.content.cloneNode(true);
+                    //on ajoute le contenu du template dans la div cible
                     cible.appendChild(contenu);
+                    //on sélectionne la class du template
                     pers = document.querySelector(".pers")
                     pers.innerHTML = element.nom + " " + element.prenom
                     pers.classList.remove("pers")
