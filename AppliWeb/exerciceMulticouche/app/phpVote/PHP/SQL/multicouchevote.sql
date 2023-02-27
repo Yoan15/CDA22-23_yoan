ded22f3d98b1527593ebe3df4b8056983cd13008
Yoan15 <yoann.deco@gmail.com>
2023-02-27 12:10:06 +0100

    27/02/2023 12:10:04,95

--- AppliWeb/exerciceMulticouche/app/phpVote/PHP/SQL/multicouchevote.sql
+++ AppliWeb/exerciceMulticouche/app/phpVote/PHP/SQL/multicouchevote.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 20 fév. 2023 à 14:53
+-- Généré le : lun. 27 fév. 2023 à 10:33
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -34,7 +34,28 @@ CREATE TABLE IF NOT EXISTS `codes` (
   `utilise` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`idCode`),
   UNIQUE KEY `code` (`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `codes`
+--
+
+INSERT INTO `codes` (`idCode`, `code`, `utilise`) VALUES
+(1, 'r4z6fds1f3', 1),
+(4, '1fg1d3g1d3', 1),
+(5, 'er4g4d2g1d', 1),
+(6, '2dsgs6r4gd', 1),
+(7, 'z5e4f5s1dg', 1),
+(8, 'e5r4hgd2d2', 1),
+(9, 'z45e421sg2', 1),
+(10, 'ze84563s13', 0),
+(11, 'dfrd4g6dg4', 0),
+(12, '5dh5f31gh1', 0),
+(13, 'r4th42f22f', 0),
+(14, '5e4tgsdf12', 0),
+(15, 'lr2u1ejuty', 0),
+(16, 'u4l2j2l2yu', 0),
+(17, 'stgs53yty3', 0);
 
 -- --------------------------------------------------------
 
@@ -62,8 +83,21 @@ CREATE TABLE IF NOT EXISTS `votes` (
   `reponse` varchar(50) NOT NULL,
   `idCode` int(11) NOT NULL,
   PRIMARY KEY (`idVote`),
-  KEY `FK_Votes_Codes` (`idCode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  UNIQUE KEY `idCode` (`idCode`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `votes`
+--
+
+INSERT INTO `votes` (`idVote`, `reponse`, `idCode`) VALUES
+(1, 'Chat', 1),
+(2, 'Chat', 4),
+(3, 'Chien', 5),
+(4, 'Chat', 6),
+(5, 'Chien', 7),
+(6, 'Chat', 8),
+(7, 'Chat', 9);
 
 --
 -- Contraintes pour les tables déchargées
