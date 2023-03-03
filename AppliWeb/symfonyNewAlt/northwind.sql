e4913a92e4c251af99b1cfdd8a443b43add6bce6
Yoan15 <yoann.deco@gmail.com>
2023-03-03 11:26:34 +0100

    northwind

--- AppliWeb/symfonyNewAlt/northwind.sql
+++ AppliWeb/symfonyNewAlt/northwind.sql
@@ -1,43 +1,330 @@
-INTO `suppliers` (`id`, `company_name`, `contact_name`, `contact_title`, `address`, `city`, `region`, `postal_code`, `country`, `phone`, `fax`, `home_page`) VALUES
-	(1, 'Exotic Liquids', 'Charlotte Cooper', 'Purchasing Manager', '49 Gilbert St.', 'London', NULL, 'EC1 4SD', 'UK', '(171) 555-2222', NULL, NULL),
-	(2, 'New Orleans Cajun Delights', 'Shelley Burke', 'Order Administrator', 'P.O. Box 78934', 'New Orleans', 'LA', '70117', 'USA', '(100) 555-4822', NULL, '#CAJUN.HTM#'),
-	(3, 'Grandma Kelly\'s Homestead', 'Regina Murphy', 'Sales Representative', '707 Oxford Rd.', 'Ann Arbor', 'MI', '48104', 'USA', '(313) 555-5735', '(313) 555-3349', NULL),
-	(4, 'Tokyo Traders', 'Yoshi Nagase', 'Marketing Manager', '9-8 Sekimai\r\nMusashino-shi', 'Tokyo', NULL, '100', 'Japan', '(03) 3555-5011', NULL, NULL),
-	(5, 'Cooperativa de Quesos \'Las Cabras\'', 'Antonio del Valle Saavedra ', 'Export Administrator', 'Calle del Rosal 4', 'Oviedo', 'Asturias', '33007', 'Spain', '(98) 598 76 54', NULL, NULL),
-	(6, 'Mayumi\'s', 'Mayumi Ohno', 'Marketing Representative', '92 Setsuko\r\nChuo-ku', 'Osaka', NULL, '545', 'Japan', '(06) 431-7877', NULL, 'Mayumi\'s (on the World Wide Web)#http://www.microsoft.com/accessdev/sampleapps/mayumi.htm#');
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 03 mars 2023 à 10:25
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
 
-INSERT INTO `products` (`id`, `product_name`, `category_id`, `quantity_per_unit`, `unit_price`, `units_in_stock`, `units_on_order`, `reorder_level`, `discontinued`, `supplier_id_id`) VALUES
-	(1, 'Chai', 1, '10 boxes x 20 bags', 18.0000, 39, 0, 10, b'0', 1),
-	(2, 'Chang', 1, '24 - 12 oz bottles', 19.0000, 17, 40, 25, b'0', 1),
-	(3, 'Aniseed Syrup', 2, '12 - 550 ml bottles', 10.0000, 13, 70, 25, b'0', 1),
-	(4, 'Chef Anton\'s Cajun Seasoning', 2, '48 - 6 oz jars', 22.0000, 53, 0, 0, b'0', 2);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `northwind`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `products_id` int(11) DEFAULT NULL,
+  `user_id_id` int(11) DEFAULT NULL,
+  `content` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_5F9E962A6C8A81A9` (`products_id`),
+  KEY `IDX_5F9E962A9D86650F` (`user_id_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE IF NOT EXISTS `customers` (
+  `id` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `company_name` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact_name` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `contact_title` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `region` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `postal_code` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `country` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(24) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fax` varchar(24) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `customers`
+--
 
 INSERT INTO `customers` (`id`, `company_name`, `contact_name`, `contact_title`, `address`, `city`, `region`, `postal_code`, `country`, `phone`, `fax`) VALUES
-	('ALFKI', 'Alfreds Futterkiste', 'Maria Anders', 'Sales Representative', 'Obere Str. 57', 'Berlin', NULL, '12209', 'Germany', '030-0074321', '030-0076545'),
-	('ANATR', 'Ana Trujillo Emparedados y helados', 'Ana Trujillo', 'Owner', 'Avda. de la Constitucin 2222', 'Mxico D.F.', NULL, '05021', 'Mexico', '(5) 555-4729', '(5) 555-3745'),
-	('ANTON', 'Antonio Moreno Taquera', 'Antonio Moreno', 'Owner', 'Mataderos  2312', 'Mxico D.F.', NULL, '05023', 'Mexico', '(5) 555-3932', NULL),
-	('AROUT', 'Around the Horn', 'Thomas Hardy', 'Sales Representative', '120 Hanover Sq.', 'London', NULL, 'WA1 1DP', 'UK', '(171) 555-7788', '(171) 555-6750'),
-	('BERGS', 'Berglunds snabbkp', 'Christina Berglund', 'Order Administrator', 'Berguvsvgen  8', 'Lule', NULL, 'S-958 22', 'Sweden', '0921-12 34 65', '0921-12 34 67');
+('ALFKI', 'Alfreds Futterkiste', 'Maria Anders', 'Sales Representative', 'Obere Str. 57', 'Berlin', NULL, '12209', 'Germany', '030-0074321', '030-0076545'),
+('ANATR', 'Ana Trujillo Emparedados y helados', 'Ana Trujillo', 'Owner', 'Avda. de la Constitucin 2222', 'Mxico D.F.', NULL, '05021', 'Mexico', '(5) 555-4729', '(5) 555-3745'),
+('ANTON', 'Antonio Moreno Taquera', 'Antonio Moreno', 'Owner', 'Mataderos  2312', 'Mxico D.F.', NULL, '05023', 'Mexico', '(5) 555-3932', NULL),
+('AROUT', 'Around the Horn', 'Thomas Hardy', 'Sales Representative', '120 Hanover Sq.', 'London', NULL, 'WA1 1DP', 'UK', '(171) 555-7788', '(171) 555-6750'),
+('BERGS', 'Berglunds snabbkp', 'Christina Berglund', 'Order Administrator', 'Berguvsvgen  8', 'Lule', NULL, 'S-958 22', 'Sweden', '0921-12 34 65', '0921-12 34 67');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+DROP TABLE IF EXISTS `doctrine_migration_versions`;
+CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20230223150840', '2023-02-23 15:08:50', 81),
+('DoctrineMigrations\\Version20230223151629', '2023-02-23 15:16:33', 48),
+('DoctrineMigrations\\Version20230223152025', '2023-02-23 15:20:28', 46),
+('DoctrineMigrations\\Version20230223152421', '2023-02-23 15:24:24', 48),
+('DoctrineMigrations\\Version20230223154428', '2023-02-23 15:44:33', 120),
+('DoctrineMigrations\\Version20230223154627', '2023-02-23 15:46:30', 51),
+('DoctrineMigrations\\Version20230223154923', '2023-02-23 15:49:28', 102),
+('DoctrineMigrations\\Version20230223155330', '2023-02-23 15:53:33', 100),
+('DoctrineMigrations\\Version20230223160054', '2023-02-23 16:00:58', 120),
+('DoctrineMigrations\\Version20230301102125', '2023-03-01 10:21:49', 605),
+('DoctrineMigrations\\Version20230301150158', '2023-03-01 15:02:02', 59),
+('DoctrineMigrations\\Version20230303083456', '2023-03-03 08:35:50', 769);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messenger_messages`
+--
+
+DROP TABLE IF EXISTS `messenger_messages`;
+CREATE TABLE IF NOT EXISTS `messenger_messages` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `available_at` datetime NOT NULL,
+  `delivered_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  KEY `IDX_75EA56E016BA31DB` (`delivered_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `employee_id` int(11) DEFAULT NULL,
+  `order_date` datetime DEFAULT NULL,
+  `required_date` datetime DEFAULT NULL,
+  `shipped_date` datetime DEFAULT NULL,
+  `ship_via` int(11) DEFAULT NULL,
+  `freight` decimal(10,4) DEFAULT NULL,
+  `ship_name` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ship_address` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ship_city` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ship_region` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ship_postal_code` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ship_country` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `customer_id_id` varchar(5) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_E52FFDEEB171EB6C` (`customer_id_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10253 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `orders`
+--
 
 INSERT INTO `orders` (`id`, `employee_id`, `order_date`, `required_date`, `shipped_date`, `ship_via`, `freight`, `ship_name`, `ship_address`, `ship_city`, `ship_region`, `ship_postal_code`, `ship_country`, `customer_id_id`) VALUES
-	(10248, 5, '1996-07-04 00:00:00', '1996-08-01 00:00:00', '1996-07-16 00:00:00', 3, 32.3800, 'Vins et alcools Chevalier', '59 rue de l-Abbaye', 'Reims', NULL, '51100', 'France', 'ANTON'),
-	(10249, 6, '1996-07-05 00:00:00', '1996-08-16 00:00:00', '1996-07-10 00:00:00', 1, 11.6100, 'Toms Spezialitten', 'Luisenstr. 48', 'Mnster', NULL, '44087', 'Germany', 'ALFKI'),
-	(10250, 4, '1996-07-08 00:00:00', '1996-08-05 00:00:00', '1996-07-12 00:00:00', 2, 65.8300, 'Hanari Carnes', 'Rua do Pao, 67', 'Rio de Janeiro', 'RJ', '05454-876', 'Brazil', 'AROUT'),
-	(10251, 3, '1996-07-08 00:00:00', '1996-08-05 00:00:00', '1996-07-15 00:00:00', 1, 41.3400, 'Victuailles en stock', '2, rue du Commerce', 'Lyon', NULL, '69004', 'France', 'BERGS'),
-	(10252, 4, '1996-07-09 00:00:00', '1996-08-06 00:00:00', '1996-07-11 00:00:00', 2, 51.3000, 'Suprmes dlices', 'Boulevard Tirou, 255', 'Charleroi', NULL, 'B-6000', 'Belgium', 'ANATR');
+(10248, 5, '1996-07-04 00:00:00', '1996-08-01 00:00:00', '1996-07-16 00:00:00', 3, '32.3800', 'Vins et alcools Chevalier', '59 rue de l-Abbaye', 'Reims', NULL, '51100', 'France', 'ANTON'),
+(10249, 6, '1996-07-05 00:00:00', '1996-08-16 00:00:00', '1996-07-10 00:00:00', 1, '11.6100', 'Toms Spezialitten', 'Luisenstr. 48', 'Mnster', NULL, '44087', 'Germany', 'ALFKI'),
+(10250, 4, '1996-07-08 00:00:00', '1996-08-05 00:00:00', '1996-07-12 00:00:00', 2, '65.8300', 'Hanari Carnes', 'Rua do Pao, 67', 'Rio de Janeiro', 'RJ', '05454-876', 'Brazil', 'AROUT'),
+(10251, 3, '1996-07-08 00:00:00', '1996-08-05 00:00:00', '1996-07-15 00:00:00', 1, '41.3400', 'Victuailles en stock', '2, rue du Commerce', 'Lyon', NULL, '69004', 'France', 'BERGS'),
+(10252, 4, '1996-07-09 00:00:00', '1996-08-06 00:00:00', '1996-07-11 00:00:00', 2, '51.3000', 'Suprmes dlices', 'Boulevard Tirou, 255', 'Charleroi', NULL, 'B-6000', 'Belgium', 'ANATR');
 
-INSERT INTO `orderdetails` (`id`, `unit_price`, `quantity`, `discount`, `product_id_id`) VALUES
-	(10248, 14.0000, 12, 0, 1),
-	(10248, 9.8000, 10, 0, 2),
-	(10248, 34.8000, 5, 0, 4),
-	(10249, 18.6000, 9, 0, 3),
-	(10249, 42.4000, 40, 0, 1),
-	(10250, 7.7000, 10, 0, 2),
-	(10250, 42.4000, 35, 0, 4),
-	(10250, 16.8000, 15, 0, 3),
-	(10251, 16.8000, 6, 0, 1),
-	(10251, 15.6000, 15, 0, 4),
-	(10251, 16.8000, 20, 0, 3),
-	(10252, 64.8000, 40, 0, 2),
-	(10252, 2.0000, 25, 0, 3),
-	(10252, 27.2000, 40, 0, 1);
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `order_details`
+--
+
+DROP TABLE IF EXISTS `order_details`;
+CREATE TABLE IF NOT EXISTS `order_details` (
+  `id` int(11) NOT NULL,
+  `unit_price` decimal(10,4) NOT NULL,
+  `quantity` smallint(6) NOT NULL,
+  `discount` double NOT NULL,
+  `product_id_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`product_id_id`),
+  KEY `IDX_845CA2C1DE18E50B` (`product_id_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `order_details`
+--
+
+INSERT INTO `order_details` (`id`, `unit_price`, `quantity`, `discount`, `product_id_id`) VALUES
+(10248, '14.0000', 12, 0, 1),
+(10248, '9.8000', 10, 0, 2),
+(10248, '34.8000', 5, 0, 4),
+(10249, '42.4000', 40, 0, 1),
+(10249, '18.6000', 9, 0, 3),
+(10250, '7.7000', 10, 0, 2),
+(10250, '16.8000', 15, 0, 3),
+(10250, '42.4000', 35, 0, 4),
+(10251, '16.8000', 6, 0, 1),
+(10251, '16.8000', 20, 0, 3),
+(10251, '15.6000', 15, 0, 4),
+(10252, '27.2000', 40, 0, 1),
+(10252, '64.8000', 40, 0, 2),
+(10252, '2.0000', 25, 0, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `quantity_per_unit` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `unit_price` decimal(10,4) DEFAULT '0.0000',
+  `units_in_stock` smallint(6) DEFAULT NULL,
+  `units_on_order` smallint(6) DEFAULT NULL,
+  `reoder_level` smallint(6) DEFAULT NULL,
+  `discontinued` tinyint(1) NOT NULL,
+  `supplier_id_id` int(11) DEFAULT NULL,
+  `picture` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_B3BA5A5AA65F9C7D` (`supplier_id_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `products`
+--
+
+INSERT INTO `products` (`id`, `product_name`, `category_id`, `quantity_per_unit`, `unit_price`, `units_in_stock`, `units_on_order`, `reoder_level`, `discontinued`, `supplier_id_id`, `picture`) VALUES
+(1, 'Chai', 1, '10 boxes x 20 bags', '18.0000', 39, 0, 10, 0, 1, NULL),
+(2, 'Chang', 1, '24 - 12 oz bottles', '19.0000', 17, 40, 25, 0, 1, NULL),
+(3, 'Aniseed Syrup', 2, '12 - 550 ml bottles', '10.0000', 13, 70, 25, 0, 1, NULL),
+(4, 'Chef Anton\'s Cajun Seasoning', 2, '48 - 6 oz jars', '22.0000', 53, 0, 0, 0, 2, NULL),
+(5, 'testModif', 2, '5', '7.0000', 31, 16, 1, 0, 4, NULL),
+(6, 'test', 1, '12', '5.0000', 31, 10, 1, 0, 5, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `suppliers`
+--
+
+DROP TABLE IF EXISTS `suppliers`;
+CREATE TABLE IF NOT EXISTS `suppliers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact_name` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `contact_title` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `region` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `postal_code` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `country` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(24) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fax` varchar(24) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `home_page` longtext COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `suppliers`
+--
+
+INSERT INTO `suppliers` (`id`, `company_name`, `contact_name`, `contact_title`, `address`, `city`, `region`, `postal_code`, `country`, `phone`, `fax`, `home_page`) VALUES
+(1, 'Exotic Liquids', 'Charlotte Cooper', 'Purchasing Manager', '49 Gilbert St.', 'London', NULL, 'EC1 4SD', 'UK', '(171) 555-2222', NULL, NULL),
+(2, 'New Orleans Cajun Delights', 'Shelley Burke', 'Order Administrator', 'P.O. Box 78934', 'New Orleans', 'LA', '70117', 'USA', '(100) 555-4822', NULL, '#CAJUN.HTM#'),
+(3, 'Grandma Kelly\'s Homestead', 'Regina Murphy', 'Sales Representative', '707 Oxford Rd.', 'Ann Arbor', 'MI', '48104', 'USA', '(313) 555-5735', '(313) 555-3349', NULL),
+(4, 'Tokyo Traders', 'Yoshi Nagase', 'Marketing Manager', '9-8 Sekimai\r\nMusashino-shi', 'Tokyo', NULL, '100', 'Japan', '(03) 3555-5011', NULL, NULL),
+(5, 'Cooperativa de Quesos \'Las Cabras\'', 'Antonio del Valle Saavedra ', 'Export Administrator', 'Calle del Rosal 4', 'Oviedo', 'Asturias', '33007', 'Spain', '(98) 598 76 54', NULL, NULL),
+(6, 'Mayumi\'s', 'Mayumi Ohno', 'Marketing Representative', '92 Setsuko\r\nChuo-ku', 'Osaka', NULL, '545', 'Japan', '(06) 431-7877', NULL, 'Mayumi\'s (on the World Wide Web)#http://www.microsoft.com/accessdev/sampleapps/mayumi.htm#');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` json NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`) VALUES
+(4, 'test2.test2@test2.fr', '[\"ROLE_ADMIN\"]', '$2y$13$OaJRZ.utJ9WE5nEyXz6DrOpZVrcRQzkxkUMRGTO5qVkwtFcCuRgAm'),
+(5, 'test3.test3@test3.fr', '[]', '$2y$13$9RWL6teFRRAIsb/43CMWJO6mj2y52aGQUeWukU5aSYxfUCg2xnFwe'),
+(6, 'test.test@test.fr', '[]', '$2y$13$DZeU2kEtqpZ4M9sR8bry2eTOv6xuc9Gk9JElN6BEvFlOJF1im6hCG');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `FK_5F9E962A6C8A81A9` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `FK_5F9E962A9D86650F` FOREIGN KEY (`user_id_id`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `FK_E52FFDEEB171EB6C` FOREIGN KEY (`customer_id_id`) REFERENCES `customers` (`id`);
+
+--
+-- Contraintes pour la table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `FK_845CA2C1DE18E50B` FOREIGN KEY (`product_id_id`) REFERENCES `products` (`id`);
+
+--
+-- Contraintes pour la table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `FK_B3BA5A5AA65F9C7D` FOREIGN KEY (`supplier_id_id`) REFERENCES `suppliers` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
