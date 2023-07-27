21bac3d173af385bbea8ff15a38dbe4b01f35155
Yoan15 <yoann.deco@gmail.com>
2023-07-27 17:49:17 +0200

    maj différents fichiers

--- AppliWeb/ExerciceIntro/PHP/Model/Manager/DBConnect.php
+++ AppliWeb/ExerciceIntro/PHP/Model/Manager/DBConnect.php
@@ -14,7 +14,7 @@ class DBConnect
         try {
             self::$db = new PDO("mysql:host=" . Parametres::get_host() . ";dbname=" . Parametres::get_dbname() . ";port=" . Parametres::get_port(), Parametres::get_user(), Parametres::get_password());
         } catch (PDOException $e) {
-            die("Erreur !: " . $e ->getMessage());
+            die("Erreur !: " . $e->getMessage());
         }
     }
 }
